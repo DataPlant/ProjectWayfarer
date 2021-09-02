d530e32ce53facfa1e14d6571921b9d091b4a5d0
DataPlant <dataplant@outlook.com>
2021-09-02 14:28:22 -0400

    installed mongoose, initialized the server.js

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,15 @@
 const express = require('express');
 const cors = require('cors')
+const cityRoutes = require('./routes/cityRoutes')
+
+const port = process.send.PORT || 4000
+const app = express()
+
+//middleware
+app.use(cors());
+app.use(express.json())
+app.use(cityRoutes)
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
